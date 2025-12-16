1ee6e80a0822ef5b92b2a2341c903ba395c73705
ziyu1221 <1720890467@qq.com>
2025-12-16 08:23:26 +0800

    Delete src directory

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-// 配置文件
-export const config = {
-  baseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:48080',
-  apiPath: import.meta.env.VITE_API_PATH || '/app-api',
-  tenantId: import.meta.env.VITE_TENANT_ID || '1',
-}
-
-export default config
-
